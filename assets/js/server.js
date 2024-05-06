92c6cc0b470dbab0525653170599d106819b295b
joanvasquez21 <joanvrivas.21@gmail.com>
2024-05-06 13:18:39 +0200

    asda

--- assets/js/server.js
+++ assets/js/server.js
@@ -9,8 +9,6 @@ const app = express();
 const puerto = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
-
-// Configuración básica de CORS
 app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'no-store');
@@ -24,7 +22,7 @@ const conexion = mysql.createConnection({
   host: 'database-2.c5l2egydpekn.us-east-1.rds.amazonaws.com',
   user: 'admin',
   password: 'DoXwEAgRJsSrij9nbeSO',
-  port: 3310, // Cambiado al puerto proporcionado por AWS RDS
+  port: 3310,
   database: 'gymproyecto',
 });
 
@@ -36,31 +34,19 @@ conexion.connect((err) => {
   console.log('Conectado a la base de datos');
 });
 
-// Configuración para servir archivos estáticos desde la carpeta 'A'
 app.use(express.static(path.join(__dirname, '../../../A')));
 
-// Crear una instancia de UserController y pasarle la conexión a la base de datos
 const userController = new UserController(conexion);
 
-// Ruta para el archivo login.html
 app.get('/view/login', (req, res) => {
   res.sendFile(path.join(__dirname, '../../../A/view', 'login.html'));
 });
 
-// Rutas sin configuración CORS específica
-/*app.post('/register', (req, res) => {
-  console.log("Solicitud POST recibida en /register");
-  userController.registerUser(req, res);
-});
-*/
 app.post('/register', (req, res) => {
   console.log('Solicitud POST recibida en /register');
-  // Aquí puedes incluir la lógica para registrar al usuario en tu base de datos si es necesario
-  // Luego, redirige al usuario a la página de inicio de sesión después de registrarse
+  console.log('Datos recibidos:', req.body);
   res.redirect('/view/login.html');
 });
-
-
 
 app.post('/login', (req, res) => {
   userController.loginUser(req, res);
